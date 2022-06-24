74b8f8ff8849aea154da02224d703719eb260509
shaik apsar <apsarsk823@gmail.com>
2022-06-24 19:06:41 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+var a= 20;
+console.log(a);
+console.log(typeof a);
+
+
+var check= "apsar"
+console.log(check);
+console.log(typeof check);
+
+
+var checkTrue= false;
+console.log(checkTrue);
+console.log(typeof checkTrue);
+
+
+var a= 7+2;
+console.log(a);
+console.log(typeof a);
+
+var a= 8+"2";
+console.log(a);
+console.log(typeof a);
+
+var a= true-5;
+console.log(a);
+console.log(typeof a);
+
+
+var a= true+5;
+console.log(a);
+console.log(typeof a);
+
+
+var a= false-5;
+console.log(a);
+console.log(typeof a);
+
+var a= false+5;
+console.log(a);
+console.log(typeof a);
+
+
+var a= true+"5";
+console.log(a);
+console.log(typeof a);
